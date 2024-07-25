eb2689fab294503524b311401e84adf0942f89ba
CristianPalta1 <cristian.palta.c@uniautonoma.edu.co>
2024-07-24 19:56:42 -0500

    iframe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <p>Holis</p>
+      <p>Esta es una prueba</p>
+      <iframe
+        src="https://ipphonecamera.deskshare.com"
+        title="IP Phone Camera"
+        frameborder="0"
+      ></iframe>
     </div>
   );
 }
